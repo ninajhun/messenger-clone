1de2d0a296a554ce875dd274a439fcc6117a930f
ninajhun <ninajhun@berkeley.edu>
2021-05-19 20:18:42 -0700

    WIP: setting up servers

--- messenger-clone/src/server.js
+++ messenger-clone/src/server.js
@@ -1,7 +1,18 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.listen(3000, () => {
-  // eslint-disable-next-line no-console
-  console.log("Express server listening on port 3000");
-});
+// // const httpServer = require("http").createServer();
+// const io = require("socket.io")(app, {
+//   // ...
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("connected!");
+// });
+
+// // httpServer.listen(3000);
+
+// app.listen(3000, () => {
+//   // eslint-disable-next-line no-console
+//   console.log("Express server listening on port 3000");
+// });
